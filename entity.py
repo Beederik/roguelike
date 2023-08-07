fd3cfeed168ea8010c2d8973b19d8efbc7e97ae4
Diederik <>
2023-08-07 10:56:10 -0500

    added entity factories and modified entity class

--- entity.py
+++ entity.py
@@ -10,12 +10,31 @@ class Entity:
     """
     A generic object to represent something in the game (players, entities, items).
     """
-    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]) -> None:
+    def __init__(self, 
+                 x: int=0, 
+                 y: int=0, 
+                 char: str="?", 
+                 color: Tuple[int, int, int]=(255, 50, 255), 
+                 name: str="<unnamed>", 
+                 blocks_movement: bool=False
+                 ) -> None:
         self.x =x
         self.y = y
         self.self = self
         self.char = char
         self.color = color
+        self.name = name
+        self.blocks_movement = blocks_movement
+
+    def spawn(self: EntityType, game_map: GameMap, x: int, y: int):
+        """
+        spawn a copy of this instance at the given location on the game map
+        """
+        clone = copy.deepcopy(self)
+        clone.x = x
+        clone.y = y
+        game_map.entities.add(clone)
+        return clone
 
     def move(self, dx: int, dy: int):
         self.self = self
