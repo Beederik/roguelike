5442d561716563add2d124faa084d369ab4674c3
Diederik <>
2023-08-10 10:58:59 -0500

    can now kill mobs

--- actions.py
+++ actions.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 from typing import Optional, Tuple, TYPE_CHECKING
+
 if TYPE_CHECKING:
     from engine import Engine
-    from entity import Entity
+    from entity import Entity, Actor
+
 
 class Action:
-    def __init__(self, entity: Entity) -> None:
+    def __init__(self, entity: Actor) -> None:
         super().__init__()
 
         self.entity = entity
@@ -25,57 +27,77 @@ class Action:
         """
         raise NotImplementedError()
 
+
 class EscapeAction(Action):
     def perform(self) -> None:
         raise SystemExit()
-    
+
+
 class WaitAction(Action):
     def perform(self) -> None:
         pass
 
+
 class ActionWithDirection(Action):
-    def __init__(self,entity: Entity, dx: int, dy: int):
+    def __init__(self, entity: Actor, dx: int, dy: int):
         super().__init__(entity)
         self.dx = dx
         self.dy = dy
 
     @property
     def dest_xy(self) -> Tuple[int, int]:
+        """return this actions destination."""
         return self.entity.x + self.dx, self.entity.y + self.dy
-    
+
     @property
     def blocking_entity(self) -> Optional[Entity]:
+        """return the blocking_entity at this actions destination."""
         return self.engine.game_map.get_blocking_entity_at_location(*self.dest_xy)
+
+    @property
+    def target_actor(self) -> Optional[Actor]:
+        """return the actor at this actions destination."""
+        return self.engine.game_map.get_actor_at_location(*self.dest_xy)
 
     def perform(self) -> None:
         raise NotImplementedError()
 
+
 class MeleeAction(ActionWithDirection):
     def perform(self) -> None:
-        
-        target = self.blocking_entity
+        target = self.target_actor
 
         if not target:
             return
-        
-        print(f"You kick the {target.name}, and it is very annoyed.")
+
+        damage = self.entity.fighter.power - target.fighter.defense
+
+        attack_desc = f"{self.entity.name.capitalize()} attacks {target.name}"
+
+        if damage > 0:
+            print (f"{attack_desc} for {damage}hp")
+            target.fighter.hp -= damage
+
+        else:
+            print (f"{attack_desc}, but is not strong enough to do damage")
 
 class MovementAction(ActionWithDirection):
     def perform(self) -> None:
         dest_x, dest_y = self.dest_xy
-       
+
         if not self.engine.game_map.in_bounds(dest_x, dest_y):
             return
         if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
             return
         if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
             return
-        
+
         self.entity.move(self.dx, self.dy)
+
 
 class BumpAction(ActionWithDirection):
     def perform(self) -> None:
-        if self.blocking_entity:
+        if self.target_actor:
             return MeleeAction(self.entity, self.dx, self.dy).perform()
 
         else:
--- components/fighter.py
+++ components/fighter.py
@@ -1,7 +1,13 @@
+from __future__ import annotations
 from components.base_component import BaseComponent
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from entity import Actor
 
 
 class Fighter(BaseComponent):
+    entity: Actor
+    
     def __init__(self, hp, defense, power) -> None:
         self.max_hp = hp
         self._hp = hp
@@ -15,3 +21,19 @@ class Fighter(BaseComponent):
     @hp.setter
     def hp(self, value: int):
         self._hp = max(0, min(value, self.max_hp))
+
+        if self._hp == 0 and self.entity.ai:
+            self.die()
+
+    def die(self) -> None:
+        if self.engine.player is self.entity:
+            print (f"You died!")
+
+        else:
+            print(f"You killed the {self.entity.name}")
+
+        self.entity.char = "%"
+        self.entity.color = (171, 1, 2)
+        self.entity.blocks_movement = False
+        self.entity.ai = None
+        self.entity.name = (f"remains of {self.entity.name}")
